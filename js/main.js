486fa0331b8d5169d18302f73717908e52920199
Brandon Argel <brandargel@gmail.com>
2021-04-09 16:08:27 -0500

    Aside bug fixed and header modified

--- js/main.js
+++ js/main.js
@@ -3,4 +3,11 @@ const btnToggle = document.getElementById("btn-toggle");
 btnToggle.addEventListener("click", function () {
   document.getElementById("aside").classList.toggle("active");
   document.getElementById("btn-toggle").classList.toggle("header__menu--close-icon");
+
+  //deshabilitar Scroll
+  if(document.getElementById("aside").classList.contains("active")){
+    document.getElementsByTagName("html")[0].style.overflow = "hidden";
+  } else {
+    document.getElementsByTagName("html")[0].style.overflow = "auto";
+  }
 });
